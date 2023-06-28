7641830e900304393c5b4ecb91b73d0ad7105dc2
Serhii <serhii.bondarenko@hal24k.com>
2023-06-28 12:13:45 +0300

    TheoryOfAlgorithms-lab1

---
+++ lab1_1(var2)/lab1(var2)/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Stack<T>
+{
+    private List<T> items;
+
+    public Stack()
+    {
+        items = new List<T>();
+    }
+
+    public void Push(T item)
+    {
+        items.Add(item);
+    }
+
+    public T Pop()
+    {
+        if (items.Count == 0)
+        {
+            throw new InvalidOperationException("Stack is empty");
+        }
+
+        int lastIndex = items.Count - 1;
+        T lastItem = items[lastIndex];
+        items.RemoveAt(lastIndex);
+        return lastItem;
+    }
+
+    public int Size()
+    {
+        return items.Count;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        TestFunctions();
+        Console.WriteLine("\n------------------------------\n");
+        TestStackInt();
+        Console.WriteLine("\n------------------------------\n");
+        TestStackString();
+    }
+    public static void TestFunctions()
+    {
+        Stack<int> stack = new Stack<int>();
+
+        // Тестовий сценарій 1: Перевірка додавання елементів до стеку
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+
+        Console.WriteLine("Stack size: " + stack.Size()); // Очікуваний результат: 3
+
+        // Тестовий сценарій 2: Перевірка вилучення елементів зі стеку
+        int poppedItem1 = stack.Pop();
+        Console.WriteLine("Popped item: " + poppedItem1); // Очікуваний результат: 3
+        Console.WriteLine("Stack size: " + stack.Size()); // Очікуваний результат: 2
+
+        int poppedItem2 = stack.Pop();
+        Console.WriteLine("Popped item: " + poppedItem2); // Очікуваний результат: 2
+        Console.WriteLine("Stack size: " + stack.Size()); // Очікуваний результат: 1
+
+        // Тестовий сценарій 3: Перевірка поведінки при спробі вилучення з пустого стеку
+        int poppedItem3;
+        try
+        {
+            poppedItem3 = stack.Pop();
+            Console.WriteLine("Popped item: " + poppedItem3);
+        }
+        catch (InvalidOperationException e)
+        {
+            Console.WriteLine("Error: " + e.Message); // Очікуваний результат: "Stack is empty"
+        }
+
+        Console.WriteLine("Stack size: " + stack.Size()); // Очікуваний результат: 0
+    }
+    public static void TestStackInt()
+    {
+        Console.WriteLine("Testing Stack<int>");
+
+        Stack<int> stack = new Stack<int>();
+
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+
+        Console.WriteLine("Stack size: " + stack.Size()); // Очікуваний результат: 3
+
+        int poppedItem = stack.Pop();
+        Console.WriteLine("Popped item: " + poppedItem); // Очікуваний результат: 3
+        Console.WriteLine("Stack size: " + stack.Size()); // Очікуваний результат: 2
+
+        Console.WriteLine();
+    }
+
+    public static void TestStackString()
+    {
+        Console.WriteLine("Testing Stack<string>");
+
+        Stack<string> stack = new Stack<string>();
+
+        stack.Push("Hello");
+        stack.Push("World");
+        stack.Push("!");
+
+        Console.WriteLine("Stack size: " + stack.Size()); // Очікуваний результат: 3
+
+        string poppedItem = stack.Pop();
+        Console.WriteLine("Popped item: " + poppedItem); // Очікуваний результат: "!"
+        Console.WriteLine("Stack size: " + stack.Size()); // Очікуваний результат: 2
+
+        Console.WriteLine();
+    }
+}
---
+++ lab1_2(var2)/lab1_2(var2)/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Stack<T>
+{
+    private List<T> items;
+
+    public Stack()
+    {
+        items = new List<T>();
+    }
+
+    public void Push(T item)
+    {
+        items.Add(item);
+    }
+
+    public T Pop()
+    {
+        if (items.Count == 0)
+        {
+            throw new InvalidOperationException("Stack is empty");
+        }
+
+        int lastIndex = items.Count - 1;
+        T lastItem = items[lastIndex];
+        items.RemoveAt(lastIndex);
+        return lastItem;
+    }
+
+    public int Size()
+    {
+        return items.Count;
+    }
+}
+public class MazeSolver
+{
+    private char[,] maze;
+    private int rows;
+    private int columns;
+    private int startX;
+    private int startY;
+    private int endX;
+    private int endY;
+    private Stack<Position> stack;
+
+    public MazeSolver(char[,] maze, int startX, int startY, int endX, int endY)
+    {
+        this.maze = maze;
+        this.rows = maze.GetLength(0);
+        this.columns = maze.GetLength(1);
+        this.startX = startX;
+        this.startY = startY;
+        this.endX = endX;
+        this.endY = endY;
+        this.stack = new Stack<Position>();
+    }
+
+    public void SolveMaze()
+    {
+        stack.Push(new Position(startX, startY));
+
+        while (stack.Size() > 0)
+        {
+            Position currentPosition = stack.Pop();
+            int x = currentPosition.X;
+            int y = currentPosition.Y;
+
+            if (IsValidMove(x, y))
+            {
+                maze[x, y] = 'x';
+
+                if (x == endX && y == endY)
+                    break;
+
+                if (IsValidMove(x - 1, y))
+                    stack.Push(new Position(x - 1, y));
+
+                if (IsValidMove(x + 1, y))
+                    stack.Push(new Position(x + 1, y));
+
+                if (IsValidMove(x, y - 1))
+                    stack.Push(new Position(x, y - 1));
+
+                if (IsValidMove(x, y + 1))
+                    stack.Push(new Position(x, y + 1));
+            }
+        }
+    }
+
+    private bool IsValidMove(int x, int y)
+    {
+        return x >= 0 && x < rows && y >= 0 && y < columns && maze[x, y] != '#' && maze[x, y] != 'x';
+    }
+
+    public void PrintMaze()
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                if (i == startX && j == startY)
+                    Console.Write("S ");
+                else if (i == endX && j == endY)
+                    Console.Write("F ");
+                else
+                    Console.Write(maze[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+public class Position
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public Position(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        char[,] maze = {
+            { '#', '#', '#', '#', '#', '#', '#', '#', '#', '#' },
+            { '#', '.', '.', '.', '#', '.', '.', '.', '.', '#' },
+            { '#', '.', '#', '.', '#', '.', '#', '#', '.', '#' },
+            { '#', '.', '#', '.', '.', '.', '.', '#', '.', '#' },
+            { '#', '#', '#', '#', '#', '#', '#', '#', '.', '#' },
+            { '#', '.', '.', '.', '.', '.', '.', '.', '.', '#' },
+            { '#', '#', '#', '#', '.', '#', '#', '#', '#', '#' },
+            { '#', '.', '.', '#', '.', '#', '.', '.', '.', '#' },
+            { '#', '#', '.', '.', '.', '.', '.', '#', '.', '#' },
+            { '#', '#', '#', '#', '#', '#', '#', '#', '#', '#' }
+        };
+
+        int startX = 1;
+        int startY = 1;
+        int endX = 8;
+        int endY = 8;
+
+        MazeSolver solver = new MazeSolver(maze, startX, startY, endX, endY);
+        solver.SolveMaze();
+        solver.PrintMaze();
+    }
+}
